6cc472c2ef338e079ee50ad42af1f8b96692527c
marco corapi <marco.corapi@outlook.it>
2021-03-17 15:57:19 +0100

    song component

---
+++ src/components/Song.js
@@ -0,0 +1,12 @@
+// This is the central part of the application that shows: song name, artist and picture
+import React from 'react';
+
+const Song = () => {
+  return(
+    <div className="song">
+      <h1>Song</h1>
+    </div>
+  )
+}
+
+export default Song;

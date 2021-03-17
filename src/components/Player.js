75915071faabb3c0e0648768cfd8eee735c0c469
marco corapi <marco.corapi@outlook.it>
2021-03-17 17:23:51 +0100

    added styling folders

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,10 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons'
 const Player = () => {
   return (
     <div className="player-container">
@@ -8,7 +14,9 @@ const Player = () => {
         <p>End Time</p>
       </div>
       <div className="play-control">
-        
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
       </div>
     </div>
   )

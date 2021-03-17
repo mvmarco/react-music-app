bcb8256fced190846f9c4d2d7c7d9b306ca792a1
marco corapi <marco.corapi@outlook.it>
2021-03-17 15:59:57 +0100

    player component

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 const Song = () => {
   return(
-    <div className="song">
-      <h1>Song</h1>
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
     </div>
   )
 }

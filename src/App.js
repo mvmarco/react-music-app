db99703bb954fbfb2e99891884a7101883ed650e
marco corapi <marco.corapi@outlook.it>
2021-03-26 21:34:26 +0100

    styling and creating library songs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 // import components
 import Song from "./components/Song.js"
 import Player from "./components/Player.js"
+import Library from "./components/Library.js";
 // import styles
 import "./styles/app.scss";
 // import data.js
 import data from "./data.js";
-
 
 /* 
   by default the first song of the array will be the first one to be played because of:
@@ -22,7 +22,12 @@ function App() {
     <div className="App">
       <h1>React Music Player</h1>
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} isPlaying={isPlaying} setisPlaying={setisPlaying} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setisPlaying={setisPlaying}
+      />
+      <Library songs={songs} />
     </div>
   );
 }
---
+++ src/components/Library.js
@@ -0,0 +1,15 @@
+import LibrarySong from "./LibrarySong.js";
+
+const Library = ({songs}) => {
+  return (
+    <div className="library">
+      <h2>Library</h2>
+      <div className="library-songs">
+        {songs.map(song =>
+          <LibrarySong song={song}/> )}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
